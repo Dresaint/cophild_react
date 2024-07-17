efa9db4704f382b8219dac0bd5d04f724581a8cb
unknown <damcoded1810@gmail.com>
2024-07-17 05:27:06 -0700

    second commit

---
+++ my_cophild_app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Welcome(){
+  return(
+    <div>
+      <h2>Welcome</h2>
+      <p>This is the beginning of React course</p>
+      <ul>
+        <li>My tutor just said about data structure of which we already know</li>
+        <li>indentation error</li>
+        <li>as a developer i want to be a able to use my skills to solve problems</li>
+      </ul>
+    </div>
+
+
+    
+  )
+}
+
+export default Welcome;
